906e4435b9c810a294a013f441295701d67351e5
mvmolloy <mvamolloy@gmail.com>
2020-06-27 11:05:32 +0000

    testing flask connection

--- app.py
+++ app.py
@@ -1,2 +1,14 @@
 import os
 from flask import Flask
+
+# new instance of flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return 'hello world'
+
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get('IP'), port=int(os.environ.get('PORT')), debug=True)
