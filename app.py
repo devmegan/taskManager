8cf9b02d16bd5cdf25b644f713b3e8ed57392f36
mvmolloy <mvamolloy@gmail.com>
2020-06-28 08:42:25 +0000

    adding env.py vars to app.py

--- app.py
+++ app.py
@@ -1,17 +1,30 @@
 import os
 from flask import Flask
-from flask_pymongo import flask_pymongo
-from bson.objectid import objectid
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+""" get env.py file for keys/URIs"""
+from os import path
+if path.exists("env.py"):
+    import env
+    print("env.py imported")
 
 # new instance of flask
 app = Flask(__name__)
 
-app.config['MONGO_DBNAME'] = 'task_manager'  # optional
-app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
+app.config['MONGO_DBNAME'] = os.environ.get('MONGO_DBNAME')
+app.config['MONGO_URI'] = os.getenv('MONGO_URI')
+
+mongo = PyMongo(app)  # create an instance of PyMongo
+
 
 @app.route('/')
 def hello():
     return 'hello world'
+
+@app.route('/get_tasks')
+def get_tasks():
+    return 'here are your tasks!'
+
 
 if __name__ == "__main__":
     app.run(host=os.environ.get('IP'), port=int(os.environ.get('PORT')), debug=True)
