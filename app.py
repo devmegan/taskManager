a9b0a797452319f9bd1c8f2c54c6671e680f564d
mvmolloy <mvamolloy@gmail.com>
2020-06-28 08:28:19 +0000

    adding app.py environ variable

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
 import os
 from flask import Flask
+from flask_pymongo import flask_pymongo
+from bson.objectid import objectid
 
 # new instance of flask
 app = Flask(__name__)
 
+app.config['MONGO_DBNAME'] = 'task_manager'  # optional
+app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
 
 @app.route('/')
 def hello():
     return 'hello world'
-
 
 if __name__ == "__main__":
     app.run(host=os.environ.get('IP'), port=int(os.environ.get('PORT')), debug=True)
