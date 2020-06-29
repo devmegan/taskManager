4d992b991846a9ade8e54f261d1b7e22aa2b175e
mvmolloy <mvamolloy@gmail.com>
2020-06-29 10:02:18 +0000

    adding delete task function

--- app.py
+++ app.py
@@ -49,7 +49,7 @@ def edit_task(task_id):
 
 @app.route('/updating_task/<task_id>', methods=["POST"])  # post hides values from url bar when being sent
 def update_task(task_id):
-    coll_tasks.update( {'_id': ObjectId(task_id)},
+    coll_tasks.update({'_id': ObjectId(task_id)},
     {
         # fetch values from the edit task form to send across
         'task_name':request.form.get('task_name'),
@@ -60,7 +60,10 @@ def update_task(task_id):
     })
     return redirect(url_for('get_tasks'))
 
-
+@app.route('/delete_task/<task_id>')
+def delete_task(task_id):
+    coll_tasks.remove({'_id': ObjectId(task_id)})
+    return redirect(url_for('get_tasks'))
 
 if __name__ == "__main__":
     app.run(host=os.environ.get('IP'),
